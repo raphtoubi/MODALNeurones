92fa02ff7e0ff243c04e7046e49721930e15b5cc
Nicolas GYS <nicolas.gys@polytechnique.edu>
2019-04-26 12:12:41 +0200

    Added saving the model at each epoch

--- INF473N_reseau_neurones.py
+++ INF473N_reseau_neurones.py
@@ -1,35 +1,62 @@
+import os
+
 import tensorflow as tf
-import tensorflow.keras as keras
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, cross_val_score
+from tensorflow import keras
 
-#importation de la data
-data = pd.read_csv('')
+#data importing -> mnist to test
+mnist = tf.keras.datasets.mnist
 
-#preprocessing --> eventuellement reshape les donnees
-X = 
-Y = 
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 2)
+Y_train = Y_train[:1000]
+Y_test = Y_test[:1000]
+
+X_train = X_train[:1000].reshape(-1, 28 * 28) / 255.0
+X_test = X_test[:1000].reshape(-1, 28 * 28) / 255.0
+
+# returns a short sequential model
+def create_model():
+    # creation du reseau de neurones
+    model = tf.keras.models.Sequential([
+
+        # input layer --> same dimension as input
+        keras.layers.Dense(units = 512, activation = tf.nn.relu, input_shape=(784,)),
+
+        # hidden layer
+        keras.layers.Dense(units = 105, activation = tf.nn.relu),
+
+        # final layer
+        keras.layers.Dense(units = 10, activation = tf.nn.softmax),
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=tf.keras.losses.sparse_categorical_crossentropy,
+                  metrics=['accuracy'])
+
+    return model
+
+# Create checkppoints
+checkpoint_path = "training_1/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path, save_weights_only=True, verbose=1,
+    # save weights, every 1-epoch
+    period=1)
+
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+
+# create a model instance
+model = create_model()
+model.summary()
+
+model.load_weights(latest)
+loss, acc = model.evaluate(X_train, Y_train)
 
 
-#creation du reseau de neurones
-model = tf.keras.models.Sequential() #Sequential pour commencer?
 
-#couche d'input --> meme dimension que l'input?
+# training
+model.fit(X_train, Y_train, epochs = 3,
+          validation_data = (X_test, Y_test),
+          callbacks = [cp_callback]) # save checkpoints at the end of each epoch
 
-model.add(tf.keras.layers.Dense(units = 105, activation = tf.nn.relu))
-model.add(tf.keras.layers.Dense(units = 53, activation = tf.nn.relu))#diviser par 2?
-
-#couche finale
-model.add(tf.keras.layers.Dense(units = 20, activation = tf.nn.softmax))
-
-#entrainement du modele
-model.fit(x_train, y_train, epochs = 3)
-
-#evaluation
-val_loss, val_acc = model.evaluate(x_test, y_test)
-
-#prediction
-model.predict()
